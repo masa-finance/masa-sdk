6fb6ed0ac6bb6caf6fa8818deb0579a9ca2922ee
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-10-27 10:32:27 +0200

    added a way to load the owner of an identity

--- src/identity/indetity.ts
+++ src/identity/indetity.ts
@@ -1,6 +1,6 @@
 import { PaymentMethod } from "../contracts";
 import { createIdentity } from "./create";
-import { loadIdentity } from "./load";
+import { loadIdentityByAddress } from "./load";
 import { burnIdentity } from "./burn";
 import { showIdentity } from "./show";
 import Masa from "../masa";
@@ -8,7 +8,7 @@ import Masa from "../masa";
 export const identity = (masa: Masa) => ({
   create: (soulName: string, duration: number, paymentMethod: PaymentMethod) =>
     createIdentity(masa, soulName, duration, paymentMethod),
-  load: (address?: string) => loadIdentity(masa, address),
+  load: (address?: string) => loadIdentityByAddress(masa, address),
   burn: () => burnIdentity(masa),
   show: (address?: string) => showIdentity(masa, address),
 });
--- src/identity/load.ts
+++ src/identity/load.ts
@@ -1,12 +1,16 @@
 import Masa from "../masa";
+import { BigNumber } from "ethers";
 
-export const loadIdentity = async (masa: Masa, address?: string) => {
-  const identityContracts = await masa.contracts.loadIdentityContracts();
+export const loadIdentityByAddress = async (
+  masa: Masa,
+  address?: string
+): Promise<BigNumber | undefined> => {
   address = address || (await masa.config.wallet.getAddress());
 
   let identityId;
 
   try {
+    const identityContracts = await masa.contracts.loadIdentityContracts();
     identityId = await identityContracts.SoulboundIdentityContract.tokenOfOwner(
       address
     );
@@ -19,4 +23,25 @@ export const loadIdentity = async (masa: Masa, address?: string) => {
   }
 
   return identityId;
+};
+
+export const loadAddressByIdentity = async (
+  masa: Masa,
+  identityId: BigNumber | number
+): Promise<string | undefined> => {
+  let address;
+  try {
+    const identityContracts = await masa.contracts.loadIdentityContracts();
+    address = await identityContracts.SoulboundIdentityContract[
+      "ownerOf(uint256)"
+    ](identityId);
+  } catch {
+    // ignore
+  }
+
+  if (!identityId) {
+    console.error(`Identity '${identityId}' does not exist`);
+  }
+
+  return address;
 };
