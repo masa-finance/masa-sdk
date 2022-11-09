1c20e7fd7a312d068cf279045249e007f9d5fa0d
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-11-09 14:55:01 +0100

    fix overusage of create random wallet

--- src/utils/environments.ts
+++ src/utils/environments.ts
@@ -1,70 +1,71 @@
 import { MasaArgs } from "../interface";
-import { createRandomWallet } from "../account";
+import { ethers } from "ethers";
 
-export const environments: ({ name: string } & MasaArgs)[] = [
+// special type for env config that makes wallet optional
+type Environment = {
+  name: string;
+  wallet?: ethers.Signer | ethers.Wallet;
+} & MasaArgs;
+
+export const environments: Environment[] = [
   {
     name: "local",
     environment: "dev",
     apiUrl: "http://localhost:4000/",
     network: "goerli",
-    wallet: createRandomWallet(),
     arweave: {
       host: "arweave.net",
       port: 443,
       protocol: "https",
       logging: false,
     },
-  },
+  } as Environment,
   {
     name: "dev",
     environment: "dev",
     apiUrl: "https://dev.middleware.masa.finance/",
     network: "goerli",
-    wallet: createRandomWallet(),
     arweave: {
       host: "arweave.net",
       port: 443,
       protocol: "https",
       logging: false,
     },
-  },
+  } as Environment,
   {
     name: "test",
     environment: "test",
     apiUrl: "https://test.middleware.masa.finance/",
     network: "goerli",
-    wallet: createRandomWallet(),
     arweave: {
       host: "arweave.net",
       port: 443,
       protocol: "https",
       logging: false,
     },
-  },
+  } as Environment,
   {
     name: "stage",
     environment: "stage",
     apiUrl: "https://beta.middleware.masa.finance/",
     network: "goerli",
-    wallet: createRandomWallet(),
     arweave: {
       host: "arweave.net",
       port: 443,
       protocol: "https",
       logging: false,
     },
-  },
+  } as Environment,
   {
     name: "production",
     environment: "production",
     apiUrl: "https://middleware.masa.finance/",
     network: "mainnet",
-    wallet: createRandomWallet(),
     arweave: {
       host: "arweave.net",
       port: 443,
       protocol: "https",
       logging: false,
     },
-  },
+  } as Environment,
 ];
