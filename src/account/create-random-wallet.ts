cd914903c6743ce4243825e7427974b918965f16
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-11-09 10:49:27 +0100

    added info when creating random wallet

--- src/account/create-random-wallet.ts
+++ src/account/create-random-wallet.ts
@@ -1,6 +1,7 @@
 import { ethers, Wallet } from "ethers";
 
 export const createRandomWallet = (): Wallet => {
+  console.info("Creating random wallet!");
   const wallet = ethers.Wallet.createRandom();
 
   wallet.connect(
