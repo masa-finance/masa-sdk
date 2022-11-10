33432623c23ff94960d7f213c98fe9084378aa16
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-11-10 16:36:41 +0100

    adopt credit score interface

--- src/interface/credit-score.ts
+++ src/interface/credit-score.ts
@@ -1,7 +1,7 @@
 export interface ICreditScore {
   name: "Masa Soulbound Credit Score v1.0.0";
   description: "A decentralized credit score";
-  image: "https://metadata.masa.finance/v1.0/identity/credit-report.png";
+  image: "https://metadata.masa.finance/v1.0/credit-score/credit-score.png";
   properties: {
     tokenId: string;
     account?: string;
