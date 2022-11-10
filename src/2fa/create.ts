6245d1fef9e2bdd327e56bee02f94b6773c8e4c4
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-11-10 10:37:48 +0100

    fix 2fa create error handling

--- src/2fa/create.ts
+++ src/2fa/create.ts
@@ -4,25 +4,32 @@ import { BigNumber } from "ethers";
 export const get2faTemplate = (phoneNumber: string, code: string) =>
   `Phone Number: ${phoneNumber} Code: ${code}`;
 
+export interface Create2FAResult {
+  success: boolean;
+  status?: string;
+  tokenId?: string | BigNumber;
+  message?: string;
+}
+
 export const create2fa = async (
   masa: Masa,
   phoneNumber: string,
   code: string
-): Promise<
-  | {
-      tokenId: string | BigNumber;
-      status: string;
-      success: boolean;
-      message: string;
-    }
-  | undefined
-> => {
+): Promise<Create2FAResult> => {
+  const result: Create2FAResult = {
+    success: false,
+  };
+
   if (await masa.session.checkLogin()) {
     const address = await masa.config.wallet.getAddress();
 
     const identityId = await masa.identity.load(address);
-    if (!identityId) return;
+    if (!identityId) {
+      result.message = "No Identity";
+      return result;
+    }
 
+    // generate message to sign
     const msg = get2faTemplate(phoneNumber, code);
 
     console.log(`Signer Address: '${address}'`);
@@ -44,19 +51,22 @@ export const create2fa = async (
     if (mint2FAData) {
       const { success, status, message, tokenId } = mint2FAData;
 
+      result.success = success;
+      result.status = status;
+      result.message = message;
+
       if (success) {
         console.log(`2FA Created: '${tokenId}'`);
-        return {
-          tokenId,
-          status,
-          success,
-          message,
-        };
+        result.tokenId = tokenId;
       } else {
         console.error(`Creating 2FA failed! '${message}'`);
       }
+
+      return result;
     }
   } else {
     console.log("Not logged in please login first");
   }
+
+  return result;
 };
