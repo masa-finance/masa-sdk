87bd72987994dcbfeb4c343e668b2b1f96c7379d
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-11-11 11:47:14 +0100

    added a way to load a single soul name details

--- src/soul-name/list.ts
+++ src/soul-name/list.ts
@@ -2,53 +2,70 @@ import Masa from "../masa";
 import { BigNumber } from "ethers";
 import { ISoulName } from "../interface";
 
+export const loadSoulNameByName = async (masa: Masa, soulName: string) => {
+  const identityContracts = await masa.contracts.loadIdentityContracts();
+
+  const tokenDetails = await identityContracts.SoulNameContract.getTokenData(
+    soulName
+  );
+
+  const owner = await identityContracts.SoulNameContract.ownerOf(
+    tokenDetails.tokenId
+  );
+
+  const tokenUri = await identityContracts.SoulNameContract[
+    "tokenURI(uint256)"
+  ](tokenDetails.tokenId);
+
+  let metadata;
+  try {
+    const metadataResponse = await masa.arweaveClient.transactions
+      .getData(tokenUri.replace("ar://", ""), {
+        decode: true,
+        string: true,
+      })
+      .catch(() => {
+        // ignore
+      });
+
+    metadata = JSON.parse(metadataResponse as string) as ISoulName;
+  } catch {
+    // ignore
+  }
+
+  return {
+    owner,
+    tokenUri,
+    tokenDetails,
+    metadata,
+  };
+};
+
 export const loadSoulNamesByIdentityId = async (
   masa: Masa,
   identityId: BigNumber
 ) => {
-  const soulNameDetails = [];
-
   const identityContracts = await masa.contracts.loadIdentityContracts();
 
   const soulNames = await identityContracts.SoulNameContract[
     "getSoulNames(uint256)"
   ](identityId);
 
-  // todo parallelize this
-  for (const nameIndex in soulNames) {
-    const tokenDetails = await identityContracts.SoulNameContract.getTokenData(
-      soulNames[nameIndex]
-    );
+  return await Promise.all(
+    soulNames.map(async (soulName, index) => {
+      const { tokenUri, tokenDetails, metadata } = await loadSoulNameByName(
+        masa,
+        soulName
+      );
 
-    const tokenUri = await identityContracts.SoulNameContract[
-      "tokenURI(uint256)"
-    ](tokenDetails.tokenId);
-
-    let metadata;
-    try {
-      const metadataResponse = await masa.arweaveClient.transactions
-        .getData(tokenUri.replace("ar://", ""), {
-          decode: true,
-          string: true,
-        })
-        .catch(() => {
-          // ignore
-        });
-
-      metadata = JSON.parse(metadataResponse as string) as ISoulName;
-    } catch {
-      // ignore
-    }
-
-    soulNameDetails.push({
-      index: nameIndex,
-      tokenUri,
-      tokenDetails,
-      metadata,
-    });
-  }
-
-  return soulNameDetails;
+      return {
+        index,
+        tokenUri,
+        tokenDetails,
+        metadata,
+      };
+    })
+  );
 };
 
 export const listSoulNames = async (masa: Masa, address?: string) => {
@@ -61,7 +78,7 @@ export const listSoulNames = async (masa: Masa, address?: string) => {
   const soulNames = await loadSoulNamesByIdentityId(masa, identityId);
 
   for (const soulName of soulNames) {
-    console.log(`\nToken: ${parseInt(soulName.index) + 1}`);
+    console.log(`\nToken: ${soulName.index + 1}`);
     console.log(`Name: ${soulName.tokenDetails.sbtName}`);
     console.log(`Token ID: ${soulName.tokenDetails.tokenId.toNumber()}`);
     console.log(`Identity ID: ${soulName.tokenDetails.identityId.toNumber()}`);
--- src/soul-name/soul-names.ts
+++ src/soul-name/soul-names.ts
@@ -3,8 +3,12 @@ import { PaymentMethod } from "../contracts";
 import Masa from "../masa";
 import { burnSoulName } from "./burn";
 import { createSoulName, getRegistrationPrice } from "./create";
-import { listSoulNames, loadSoulNamesByIdentityId } from "./list";
-import { sendSoulName } from "./send"
+import {
+  listSoulNames,
+  loadSoulNameByName,
+  loadSoulNamesByIdentityId,
+} from "./list";
+import { sendSoulName } from "./send";
 
 export const soulNames = (masa: Masa) => ({
   getRegistrationPrice: (
@@ -15,6 +19,7 @@ export const soulNames = (masa: Masa) => ({
   list: (address?: string) => listSoulNames(masa, address),
   loadSoulNamesByIdentityId: (identityId: BigNumber) =>
     loadSoulNamesByIdentityId(masa, identityId),
+  loadSoulNameByName: (soulName: string) => loadSoulNameByName(masa, soulName),
   create: (soulName: string, duration: number, paymentMethod: PaymentMethod) =>
     createSoulName(masa, soulName, duration, paymentMethod),
   burn: (soulName: string) => burnSoulName(masa, soulName),
