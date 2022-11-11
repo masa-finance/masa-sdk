45d53be637db1a27577bc585b24c9d6e5efc74e8
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-11-11 12:56:58 +0100

    return more balances

--- src/account/get-balances.ts
+++ src/account/get-balances.ts
@@ -5,30 +5,49 @@ import Masa from "../masa";
 export const getBalances = async (masa: Masa, address: string) => {
   if (!masa.config.wallet.provider) return;
 
+  const identityContracts = await masa.contracts.loadIdentityContracts();
   const contractAddresses = addresses[masa.config.network];
+  const signerAddress = masa.config.wallet.getAddress();
 
-  const [ethBalance, masaBalance, usdcBalance, wethBalance] = await Promise.all(
-    [
-      masa.config.wallet.provider.getBalance(address),
-      MASA__factory.connect(
-        contractAddresses.MASA,
-        masa.config.wallet.provider
-      ).balanceOf(address),
-      MASA__factory.connect(
-        contractAddresses.USDC,
-        masa.config.wallet.provider
-      ).balanceOf(address),
-      MASA__factory.connect(
-        contractAddresses.WETH,
-        masa.config.wallet.provider
-      ).balanceOf(address),
-    ]
-  );
-
-  return {
+  const [
     ethBalance,
     masaBalance,
     usdcBalance,
     wethBalance,
+    identityBalance,
+    soulNameBalance,
+    soulboundCreditScoreBalance,
+    soulbound2FABalance,
+  ] = await Promise.all([
+    masa.config.wallet.provider.getBalance(address),
+    MASA__factory.connect(
+      contractAddresses.MASA,
+      masa.config.wallet.provider
+    ).balanceOf(address),
+    MASA__factory.connect(
+      contractAddresses.USDC,
+      masa.config.wallet.provider
+    ).balanceOf(address),
+    MASA__factory.connect(
+      contractAddresses.WETH,
+      masa.config.wallet.provider
+    ).balanceOf(address),
+    identityContracts.SoulboundIdentityContract.balanceOf(signerAddress),
+    identityContracts.SoulNameContract.balanceOf(signerAddress),
+    identityContracts.SoulboundCreditReportContract.balanceOf(signerAddress),
+    identityContracts.Soulbound2FA.balanceOf(signerAddress),
+  ]);
+
+  const balances = {
+    ethBalance,
+    masaBalance,
+    usdcBalance,
+    wethBalance,
+    identityBalance,
+    soulNameBalance,
+    soulboundCreditScoreBalance,
+    soulbound2FABalance,
   };
+
+  return balances;
 };
