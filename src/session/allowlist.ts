620737fa2b6d8ae0a1447c8c6bcae5efcea1783b
hide-on-bush-x <hideonbushx123@gmail.com>
2022-12-20 14:16:43 -0300

    Adding allowlist check to SDK

---
+++ src/session/allowlist.ts
@@ -0,0 +1,16 @@
+import Masa from "../masa";
+
+export const checkAllowlist = async (masa: Masa): Promise<boolean> => {
+  let loggedIn = false;
+
+  const checkSessionResponse = await masa.client.sessionCheck();
+  if (checkSessionResponse) {
+    console.log(`User ID: '${checkSessionResponse.user.userId}'`);
+    console.log(`Signer Address: '${await masa.config.wallet.getAddress()}'`);
+    console.log("\n");
+
+    loggedIn = true;
+  }
+
+  return loggedIn;
+};
