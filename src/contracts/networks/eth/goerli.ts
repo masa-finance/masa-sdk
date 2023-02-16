cefc87019a4f99d0af5b61f15c2a5ff68bd962a9
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-02-16 11:38:58 +0100

    added masa token

--- src/contracts/networks/eth/goerli.ts
+++ src/contracts/networks/eth/goerli.ts
@@ -1,11 +1,14 @@
+// identity
 import SoulboundIdentity from "@masa-finance/masa-contracts-identity/deployments/goerli/SoulboundIdentity.json";
 import SoulboundCreditScore from "@masa-finance/masa-contracts-identity/deployments/goerli/SoulboundCreditScore.json";
 import SoulName from "@masa-finance/masa-contracts-identity/deployments/goerli/SoulName.json";
 import SoulStore from "@masa-finance/masa-contracts-identity/deployments/goerli/SoulStore.json";
 import SoulLinker from "@masa-finance/masa-contracts-identity/deployments/goerli/SoulLinker.json";
 import SoulboundGreen from "@masa-finance/masa-contracts-identity/deployments/goerli/SoulboundGreen.json";
+// token
+import MasaToken from "@masa-finance/masa-token/deployments/mainnet/MasaToken.json";
 
-export const MASA = undefined;
+export const MASA = MasaToken.address;
 export const USDC = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F";
 export const WETH = undefined;
 
--- src/contracts/networks/eth/mainnet.ts
+++ src/contracts/networks/eth/mainnet.ts
@@ -1,11 +1,14 @@
+// identity
 import SoulboundIdentity from "@masa-finance/masa-contracts-identity/deployments/mainnet/SoulboundIdentity.json";
 import SoulboundCreditScore from "@masa-finance/masa-contracts-identity/deployments/mainnet/SoulboundCreditScore.json";
 import SoulName from "@masa-finance/masa-contracts-identity/deployments/mainnet/SoulName.json";
 import SoulStore from "@masa-finance/masa-contracts-identity/deployments/mainnet/SoulStore.json";
 import SoulLinker from "@masa-finance/masa-contracts-identity/deployments/mainnet/SoulLinker.json";
 import SoulboundGreen from "@masa-finance/masa-contracts-identity/deployments/mainnet/SoulboundGreen.json";
+// token
+import MasaToken from "@masa-finance/masa-token/deployments/mainnet/MasaToken.json";
 
-export const MASA = undefined;
+export const MASA = MasaToken.address;
 export const USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 export const WETH = undefined;
 
