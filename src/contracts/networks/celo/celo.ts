0ef47fc58aaf75078dc09c9fcae44f5ca2b71079
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-03-03 10:56:27 +0100

    added celo addresses

--- src/contracts/networks/celo/celo.ts
+++ src/contracts/networks/celo/celo.ts
@@ -1,7 +1,13 @@
+import SoulboundIdentity from "@masa-finance/masa-contracts-identity/deployments/celo/SoulboundIdentity.json";
+import SoulName from "@masa-finance/masa-contracts-identity/deployments/celo/SoulName.json";
+import SoulStore from "@masa-finance/masa-contracts-identity/deployments/celo/SoulStore.json";
 import SoulboundGreen from "@masa-finance/masa-contracts-identity/deployments/celo/SoulboundGreen.json";
 
 export const MASA = undefined;
 export const USDC = undefined;
 export const WETH = undefined;
 
+export const SoulboundIdentityAddress = SoulboundIdentity.address;
+export const SoulNameAddress = SoulName.address;
+export const SoulStoreAddress = SoulStore.address;
 export const SoulboundGreenAddress = SoulboundGreen.address;
