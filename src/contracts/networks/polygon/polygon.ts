6ec8a841ee60439bc6c63284b469d0a3a8bcee7d
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-04-19 15:25:55 +0200

    added credit score + identity to polygon mainnet

--- src/contracts/networks/polygon/polygon.ts
+++ src/contracts/networks/polygon/polygon.ts
@@ -1,6 +1,12 @@
+import { address as SoulboundIdentityAddress } from "@masa-finance/masa-contracts-identity/deployments/polygon/SoulboundIdentity.json";
+import { address as SoulboundCreditScoreAddress } from "@masa-finance/masa-contracts-identity/deployments/polygon/SoulboundCreditScore.json";
+import { address as SoulStoreAddress } from "@masa-finance/masa-contracts-identity/deployments/polygon/SoulStore.json";
 import { address as SoulboundGreenAddress } from "@masa-finance/masa-contracts-identity/deployments/polygon/SoulboundGreen.json";
 import { Addresses } from "../../addresses";
 
 export const polygon: Addresses = {
+  SoulboundIdentityAddress,
+  SoulboundCreditScoreAddress,
+  SoulStoreAddress,
   SoulboundGreenAddress,
 };
