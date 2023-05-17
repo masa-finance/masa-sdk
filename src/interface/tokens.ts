8d17a0a69d79e036bf3220d3879da9b2cc9ccf57
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-05-17 11:11:06 +0200

    added comments added matic and bnb as native supported currencies

--- src/interface/tokens.ts
+++ src/interface/tokens.ts
@@ -1,7 +1,27 @@
-export const erc20Currencies = ["MASA", "WETH", "G$", "USDC", "cUSD"] as const;
+export const erc20Currencies = [
+  // Masa
+  "MASA",
+  // wrapped eth
+  "WETH",
+  // good dollar
+  "G$",
+  // USDC
+  "USDC",
+  // celo USD
+  "cUSD",
+] as const;
 export type ERC20Currencies = (typeof erc20Currencies)[number];
 
-export const nativeCurrencies = ["ETH", "CELO"] as const;
+export const nativeCurrencies = [
+  // ethereum
+  "ETH",
+  // celo
+  "CELO",
+  // matic
+  "MATIC",
+  // binance smart chain
+  "BNB",
+] as const;
 export type NativeCurrencies = (typeof nativeCurrencies)[number];
 
 export const isNativeCurrency = (
