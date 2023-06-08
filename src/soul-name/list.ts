e6a1c9ae9619eae4ed4c02fffe28f9567bbc12b9
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-06-08 11:33:27 +0200

    fix No soulnames found for 'undefined' error

--- src/soul-name/list.ts
+++ src/soul-name/list.ts
@@ -3,6 +3,12 @@ import { SoulNameDetails } from "../interface";
 import { loadSoulNameDetailsByAddress } from "./load";
 import { printSoulName } from "./helpers";
 
+/**
+ * list soul names
+ *
+ * @param masa
+ * @param address
+ */
 export const listSoulNames = async (
   masa: Masa,
   address?: string
@@ -11,10 +17,17 @@ export const listSoulNames = async (
   return await loadSoulNameDetailsByAddress(masa, address);
 };
 
+/**
+ * list soul names and print them
+ *
+ * @param masa
+ * @param address
+ */
 export const listSoulNamesAndPrint = async (
   masa: Masa,
   address?: string
 ): Promise<SoulNameDetails[]> => {
+  address = address || (await masa.config.signer.getAddress());
   const soulNames = await listSoulNames(masa, address);
 
   if (soulNames.length > 0) {
