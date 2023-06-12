6391b943d6e436c983798fb6c4203d1b8f1c693e
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-06-12 09:26:29 +0200

    refactor

--- src/contracts/modules/index.ts
+++ src/contracts/modules/index.ts
@@ -2,6 +2,6 @@ export * from "./contract-factory";
 export * from "./credit-score";
 export * from "./green";
 export * from "./identity";
-export * from "./sbt/";
+export * from "./sbt";
 export * from "./soul-linker";
 export * from "./soul-name";
--- src/interface/index.ts
+++ src/interface/index.ts
@@ -4,5 +4,5 @@ export * from "./masa-base";
 export * from "./masa-interface";
 export * from "./masa-linkable";
 export * from "./session";
-export * from "./token/";
+export * from "./token";
 export * from "./tokens";
