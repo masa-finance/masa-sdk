d46adb77a4fcac750f7edf4183a530cf2a801d7e
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-06-12 15:59:01 +0200

    fix antipatterns

--- src/utils/unpack-session-id.ts
+++ src/utils/unpack-session-id.ts
@@ -1,10 +1,3 @@
 export const unpackSessionId = (cookie?: string): string | undefined => {
-  let result = undefined;
-
-  if (!cookie) {
-    return result;
-  }
-
-  result = cookie?.split(";")[0].split("=")[1];
-  return result;
+  return cookie?.split(";")[0].split("=")[1];
 };
