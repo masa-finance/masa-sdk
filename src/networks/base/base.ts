6b782094d62edc451bb1e63e73ad4f586b88e773
Im Khem <im@masa.finance>
2023-08-08 16:10:41 -0700

    updated base addresses

--- src/networks/base/base.ts
+++ src/networks/base/base.ts
@@ -1,3 +1,15 @@
+import { base as baseAddresses } from "@masa-finance/masa-contracts-identity/addresses.json";
+
 import type { Addresses } from "../../interface";
 
-export const base: Addresses = {};
+const {
+  SoulboundIdentity: SoulboundIdentityAddress,
+  SoulName: SoulNameAddress,
+  SoulStore: SoulStoreAddress,
+} = baseAddresses;
+
+export const base: Addresses = {
+  SoulboundIdentityAddress,
+  SoulNameAddress,
+  SoulStoreAddress,
+};
