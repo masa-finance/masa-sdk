1899c8257e578bde89ceb7cff53356594b37bb3f
Nolan Jacobson <50815660+nolanjacobson@users.noreply.github.com>
2024-03-15 11:31:52 -0400

    add export

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -1,4 +1,5 @@
 export * from "./contract-modules";
 export * from "./load-Identity-contracts";
+export * from "./load-marketplace-contracts";
 export * from "./masa-contracts";
 export * from "./static-JsonRpcProvider";
