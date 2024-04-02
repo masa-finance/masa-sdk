d21659945477116c2a0a25a77cf9ea0ae8ddde2f
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-04-02 11:33:21 +0200

    feature (bridge): add support for bsc testnet

--- src/networks/bsc/bsctest.ts
+++ src/networks/bsc/bsctest.ts
@@ -1,9 +1,13 @@
 import { bsctest as bsctestAddresses } from "@masa-finance/masa-contracts-identity/addresses.json";
+import { bsctest as bsctestAddressesMasaToken } from "@masa-finance/masa-token/addresses.json";
 
 import type { Addresses } from "../../interface";
 
 const { SoulboundGreen: SoulboundGreenAddress } = bsctestAddresses;
 
 export const bsctest: Addresses = {
+  tokens: {
+    MASA: bsctestAddressesMasaToken.MasaTokenOFT,
+  },
   SoulboundGreenAddress,
 };
