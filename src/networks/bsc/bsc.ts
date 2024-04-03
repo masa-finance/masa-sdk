3e5bb165197076103059af2bc4ff4d086f5925db
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-04-03 14:30:34 +0200

    feature (token): added masa token on bsc mainnet

--- src/networks/bsc/bsc.ts
+++ src/networks/bsc/bsc.ts
@@ -1,9 +1,13 @@
 import { bsc as bscAddresses } from "@masa-finance/masa-contracts-identity/addresses.json";
+import { bsc as bscAddressesMasaToken } from "@masa-finance/masa-token/addresses.json";
 
 import type { Addresses } from "../../interface";
 
 const { SoulboundGreen: SoulboundGreenAddress } = bscAddresses;
 
 export const bsc: Addresses = {
+  tokens: {
+    MASA: bscAddressesMasaToken.MasaTokenOFT,
+  },
   SoulboundGreenAddress,
 };
