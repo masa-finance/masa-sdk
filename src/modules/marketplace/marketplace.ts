0ee6ec7a7ac353f6323e9bec5285b7413c7e9ed1
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-04-10 16:08:24 +0200

    feature: make marketplace a base module to report contract status

--- src/modules/marketplace/marketplace.ts
+++ src/modules/marketplace/marketplace.ts
@@ -1,8 +1,13 @@
-import { MasaBase } from "../../masa-base";
+import type { MasaInterface } from "../../interface";
+import { MasaModuleBase } from "../masa-module-base";
 import { claimAllRewards } from "./claimAllRewards";
 import { stakeAll } from "./stakeAll";
 
-export class MasaMarketplace extends MasaBase {
+export class MasaMarketplace extends MasaModuleBase {
+  constructor(masa: MasaInterface) {
+    super(masa, masa.contracts.instances.DataPointsMulti);
+  }
+
   stakeAll = () => stakeAll(this.masa);
   claimAllRewards = () => claimAllRewards(this.masa);
 }
