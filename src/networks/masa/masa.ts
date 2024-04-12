cd759e19a9e2022da922c2e65b7d3864740e9f25
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-04-12 16:55:49 +0200

    fix: map contract addresses

--- src/networks/masa/masa.ts
+++ src/networks/masa/masa.ts
@@ -1,9 +1,13 @@
 import { masa as masaAddresses } from "@masa-finance/masa-contracts-identity/addresses.json";
+import { masa as masaAddressesMarketplace } from "@masa-finance/masa-contracts-marketplace/addresses.json";
 
 import type { Addresses } from "../../interface";
+
+const { DataPointsMulti: DataPointsMultiAddress } = masaAddressesMarketplace;
 
 const { SoulboundIdentity: SoulboundIdentityAddress } = masaAddresses;
 
 export const masa: Addresses = {
   SoulboundIdentityAddress,
+  DataPointsMultiAddress,
 };
