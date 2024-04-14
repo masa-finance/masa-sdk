2772ea7f18b3d5e644dc5072cac7c1b763cf2fa4
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-04-14 07:48:28 +0200

    feature: masa token mainnet

--- src/networks/masa/masa.ts
+++ src/networks/masa/masa.ts
@@ -1,5 +1,6 @@
 import { masa as masaAddresses } from "@masa-finance/masa-contracts-identity/addresses.json";
 import { masa as masaAddressesMarketplace } from "@masa-finance/masa-contracts-marketplace/addresses.json";
+import { masa as masaAddressesMasaToken } from "@masa-finance/masa-token/addresses.json";
 
 import type { Addresses } from "../../interface";
 
@@ -8,6 +9,9 @@ const { DataPointsMulti: DataPointsMultiAddress } = masaAddressesMarketplace;
 const { SoulboundIdentity: SoulboundIdentityAddress } = masaAddresses;
 
 export const masa: Addresses = {
+  tokens: {
+    MASA: masaAddressesMasaToken.MasaTokenNativeOFT,
+  },
   SoulboundIdentityAddress,
   DataPointsMultiAddress,
 };
