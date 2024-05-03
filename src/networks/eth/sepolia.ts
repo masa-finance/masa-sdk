97ee1304a5b2126a35d99609e63d0ba3671fa4b1
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-05-03 17:17:44 +0200

    feature (staking): update staking contracts, map staking contract

--- src/networks/eth/sepolia.ts
+++ src/networks/eth/sepolia.ts
@@ -1,10 +1,14 @@
 // token
+import { sepolia as sepoliaAddressesMasaStaking } from "@masa-finance/masa-contracts-staking/addresses.json";
 import { sepolia as sepoliaAddressesMasaToken } from "@masa-finance/masa-token/addresses.json";
 
 import type { Addresses } from "../../interface";
+
+const { MasaStaking: MasaStakingAddress } = sepoliaAddressesMasaStaking;
 
 export const sepolia: Addresses = {
   tokens: {
     MASA: sepoliaAddressesMasaToken.MasaToken,
   },
+  MasaStakingAddress,
 };
