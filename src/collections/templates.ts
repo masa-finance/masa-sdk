6df4d21be0ab63df63c1ae96da56f87a55f45f84
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-05-17 10:02:46 +0200

    feat (login template): added a rotation template to rotate the login template

--- src/collections/templates.ts
+++ src/collections/templates.ts
@@ -1,6 +1,7 @@
 export const Templates = {
   /**
-   * The Masa Finance Login Template used for authenticating with the middleware
+   * The Masa Finance Login Template used for authenticating with masa-express
+   *
    * @param challenge The challenge received from the login process
    * @param expires The session expiration date received from the login process
    */
@@ -10,6 +11,22 @@ export const Templates = {
   ) => `Welcome to 🌽Masa Finance!
 
 Login with your soulbound web3 identity to unleash the power of DeFi.
+
+Your signature is valid till: ${expires}.
+Challenge: ${challenge}`,
+
+  /**
+   * The Masa Finance Login rotation Template used for authenticating with masa-express
+   *
+   * @param challenge The challenge received from the login process
+   * @param expires The session expiration date received from the login process
+   */
+  loginTemplateNext: (
+    challenge: string,
+    expires: string,
+  ) => `Welcome to the Masa Network 🪐
+
+Login to the Masa App to join the Masa AI Data Network. Start staking MASA and unleash the power of AI.
 
 Your signature is valid till: ${expires}.
 Challenge: ${challenge}`,
