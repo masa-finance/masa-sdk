504cb8d0b03f0b12d07ce8868c32d50230686bca
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-06-27 11:21:48 +0200

    feat (staking on aurora): mapped staking contract address

--- src/networks/aurora/auroratest.ts
+++ src/networks/aurora/auroratest.ts
@@ -1,10 +1,13 @@
-// token
+import { auroratest as auroratestAddressesMasaStaking } from "@masa-finance/masa-contracts-staking/addresses.json";
 import { auroratest as auroratestAddressesMasaToken } from "@masa-finance/masa-token/addresses.json";
 
 import type { Addresses } from "../../interface";
+
+const { MasaStaking: MasaStakingAddress } = auroratestAddressesMasaStaking;
 
 export const auroratest: Addresses = {
   tokens: {
     MASA: auroratestAddressesMasaToken.MasaTokenOFT,
   },
+  MasaStakingAddress,
 };
