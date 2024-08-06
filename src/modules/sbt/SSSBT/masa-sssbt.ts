fd1bc5d55eac8429b798c3ab607afaac1869064f
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-08-06 15:44:43 +0200

    chore (remove sbt create): removed all deploy, mint and sign methods

--- src/modules/sbt/SSSBT/masa-sssbt.ts
+++ src/modules/sbt/SSSBT/masa-sssbt.ts
@@ -3,44 +3,9 @@ import { ReferenceSBTSelfSovereign__factory } from "@masa-finance/masa-contracts
 
 import type { ContractFactory } from "../../../interface/contract-factory";
 import { MasaBase } from "../../../masa-base";
-import { deploySSSBT } from "./deploy";
 import { MasaSSSBTWrapper } from "./masa-sssbt-wrapper";
 
 export class MasaSSSBT extends MasaBase {
-  /**
-   *
-   * @param name
-   * @param symbol
-   * @param baseTokenUri
-   * @param authorityAddress
-   * @param limit
-   * @param adminAddress
-   */
-  deploy = ({
-    name,
-    symbol,
-    baseTokenUri,
-    limit = 1,
-    authorityAddress,
-    adminAddress,
-  }: {
-    name: string;
-    symbol: string;
-    baseTokenUri: string;
-    limit?: number;
-    authorityAddress: string;
-    adminAddress?: string;
-  }) =>
-    deploySSSBT({
-      masa: this.masa,
-      name,
-      symbol,
-      baseTokenUri,
-      limit,
-      authorityAddress,
-      adminAddress,
-    });
-
   /**
    *
    * @param contract
