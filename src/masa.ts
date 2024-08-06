bb9cdc0e9dc8398b27556a1db7e4a16034520828
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2024-08-06 14:50:00 +0200

    chore (middlware login): Use analytics api to login from now on

--- src/masa.ts
+++ src/masa.ts
@@ -49,7 +49,7 @@ export class Masa implements MasaInterface {
   public constructor({
     cookie,
     signer,
-    apiUrl = "https://middleware.masa.finance",
+    apiUrl = "https://api.cookiemonster.masa.finance",
     environment = "production",
     networkName = "ethereum",
     arweave = {
